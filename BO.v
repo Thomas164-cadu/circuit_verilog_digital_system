61b35c260d4cf7b06b857cc4268da33f00b2fdfe
evellyncamatti <evellyncco@gmail.com>
2022-03-19 15:07:00 -0300

    Criando BO
    Criação do BO

---
+++ BO.v
@@ -0,0 +1,20 @@
+module BO (
+    input [15:0] A,
+    input [15:0] B,
+    input [15:0] C,
+    input MO,
+    input [7:0] X,
+    input clk,
+    input LX,
+    input RSTX,
+    input M1,
+    input M2,
+    input LH,
+    input clkH,
+    input RSTH,
+    input LS,
+    input clkS,
+    output [15:0] Pronto 
+); 
+    
+endmodule
